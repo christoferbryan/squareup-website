ffd239f6ff20f6c50f4db363a6fb800131225c3e
Christofer Bryan <christoferbryan45@gmail.com>
2024-05-29 21:01:22 +0700

    feat(main): added testimony object

---
+++ src/app/utils/Person.ts
@@ -0,0 +1,25 @@
+const PersonData = [
+    {
+      header : 'SquareUp has been Instrumental in Transforming our Online Presence.',
+      desc : "Their team's expertise in web development and design resulted in a visually stunning and user-friendly e-commerce platform. Our online sales have skyrocketed, and we couldn't be happier.",
+      photo: '/profile-1.svg',
+      name: 'Finn Rasmussen',
+      position: 'CEO of Zap Solutions',
+    },
+    {
+        header : 'SquareUp has been Instrumental in Transforming our Online Presence.',
+      desc : "Their team's expertise in web development and design resulted in a visually stunning and user-friendly e-commerce platform. Our online sales have skyrocketed, and we couldn't be happier.",
+      photo: '/profile-2.svg',
+      name: 'Meghan Webb',
+      position: 'CTO of ABC Corporation',
+    },
+    {
+        header : 'SquareUp developed a comprehensive booking and reservation system for our event management company',
+      desc : "Their attention to detail and commitment to delivering a user-friendly platform was evident throughout the project. The system has streamlined our operations and enhanced our clients' event experiences.",
+      photo: '/profile-3.svg',
+      name: 'John Smith',
+      position: 'CFO of Super Corp.',
+    },
+]
+
+export default PersonData;
