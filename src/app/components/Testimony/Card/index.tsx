2cfe22ae57457abf56a9a6b0c490f742cae94898
Christofer Bryan <christoferbryan45@gmail.com>
2024-05-30 22:38:26 +0700

    fix(main): fixed text size of testimony card for large display

--- src/app/components/Testimony/Card/index.tsx
+++ src/app/components/Testimony/Card/index.tsx
@@ -14,8 +14,8 @@ const TestimonyCard : React.FC<TestimonyCardProps> = ({ header, desc, photo, nam
             <div className="flex flex-col rounded-md justify-center bg-zinc-900 w-[350px] h-[380px] px-[25px] py-[40px] gap-y-[25px] border-2 border-zinc-600
             lg:w-[426.67px]">
                 <div className="flex flex-col text-left gap-y-[20px]">
-                    <h2 className="font-medium text-[18px] text-[#D8FF99]">{header}</h2>
-                    <p className="text-[#E6E6E6] text-[14px] font-normal">
+                    <h2 className="font-medium text-[18px] text-[#D8FF99] lg:text-[20px]">{header}</h2>
+                    <p className="text-[#E6E6E6] text-[14px] font-normal lg:text-[16px]">
                         {desc}
                     </p>
                 </div>
