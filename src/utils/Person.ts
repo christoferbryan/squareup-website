4684d5cbac36df400d412fc3cc84289ff0f9492c
Christofer Bryan <christoferbryan45@gmail.com>
2024-05-30 15:11:03 +0700

    added testimony people objects

--- src/utils/Person.ts
+++ src/utils/Person.ts
@@ -20,6 +20,28 @@ const PersonData = [
       name: 'John Smith',
       position: 'CFO of Super Corp.',
     },
+    {
+      header : 'ProTech Solutions turned to SquareUp to automate our workflow',
+    desc : "They delivered an exceptional custom software solution. The system has significantly increased our productivity and reduced manual errors. SquareUp's expertise and professionalism have made them a trusted technology partner.",
+    photo: '/profile-4.svg',
+    name: 'Laura Adams',
+    position: 'COO of ProTech Solutions',
+  },
+  {
+    header : 'SquareUp designed and developed a captivating web portal for showcasing our real estate listings.',
+  desc : "The platform is visually appealing and easy to navigate, allowing potential buyers to find their dream homes effortlessly. SquareUp's expertise in the real estate industry is unmatched.",
+  photo: '/profile-5.svg',
+  name: 'Michael Anderson',
+  position: 'Founder of Dreams Reality',
+},
+{
+  header : 'FitLife Tracker wanted a mobile app that tracked fitness activities and provided personalized workout plans.',
+desc : "SquareUp's team developed an intuitive and feature-rich app that has helped our users stay motivated and achieve their fitness goals. We highly recommend SquareUp for any health and fitness app development needs.",
+photo: '/profile-6.svg',
+name: 'Emily Turner',
+position: 'CEO of FitLife Tracker',
+},
+
 ]
 
 export default PersonData;
