c09fafe00071f52b77507933690fdc71bef5ec8c
Christofer Bryan <christoferbryan45@gmail.com>
2024-05-31 18:06:08 +0700

    fix(main): fixed vertical padding of Accordion component and edited descriptions

--- src/app/components/Faq/Accordion/index.tsx
+++ src/app/components/Faq/Accordion/index.tsx
@@ -19,16 +19,16 @@ const Accordion : React.FC<AccordionProps> = ({ number, header, desc }) => {
 
     return (
         <>
-            <div className="relative flex flex-col px-[25px] py-[25px] gap-y-[25px] bg-zinc-900 border-x border-b border-zinc-600 w-[350px] 
-             lg:w-[640px] lg:px-[40px]">
+            <div className={`${isOpen ? '' : 'h-[120px] lg:h-[110px]'} relative flex flex-col px-[25px] py-[15px] gap-y-[25px] bg-zinc-900 border-x border-b border-zinc-600 w-[350px] 
+             lg:w-[640px] lg:px-[40px]`}>
                 <div className="flex items-center gap-x-[20px] w-[270px] lg:w-[450px] lg:gap-x-[30px]">
                     <h2 className={`font-normal text-[30px] ${isOpen ? 'text-[#C5FF66]' : 'text-white'}`}>{number}</h2>
-                    <h2 className={`font-medium text-[16px] ${isOpen ? 'text-[#C5FF66]' : 'text-white'}`}>{header}</h2>
+                    <h2 className={`font-medium text-[16px] lg:text-[18px] ${isOpen ? 'text-[#C5FF66]' : 'text-white'}`}>{header}</h2>
                 </div>
-                <Image className="cursor-pointer absolute right-4 top-9" onClick={toggleAccordion} src={isOpen ? closeIcon : plusIcon} alt="close-icon" width={26} height={26} />
+                <Image className="cursor-pointer absolute right-4 top-9" onClick={toggleAccordion} src={isOpen ? closeIcon : plusIcon} alt="close-open-icon" width={26} height={26} />
                 {
                     isOpen && (
-                        <div className="text-[14px] text-[#E6E6E6] font-normal">
+                        <div className="text-[14px] lg:text-[16px] text-[#E6E6E6] font-normal">
                             {desc}
                         </div>       
                     )
--- src/utils/Faq.ts
+++ src/utils/Faq.ts
@@ -7,37 +7,37 @@ const FaqData = [
     {
         number: '02',
         header : 'How can SquareUp help my business?',
-        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+        desc : 'SquareUp is dedicated to helping your business thrive by providing tailored solutions designed to meet your specific needs. We offer a comprehensive range of services including design, engineering, and project management all aimed at enhancing your operational efficiency and driving growth.',
     },
     {
         number: '03',
         header : 'What industries does SquareUp work with?',
-        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+        desc : "Our business works with a diverse range of industries, leveraging our expertise to deliver customized solutions tailored to each sector's unique needs. We have successfully partnered with clients in, but not limited to healthcare, banking and finance, retail, technology, education, and manufacturing industries. Our experience across various industries allows us to understand and address the specific challenges and opportunities each sector presents. Whether you are in a highly regulated industry like healthcare or a fast-paced field like technology, we have the knowledge and skills to help your business thrive.",
     },
     {
         number: '04',
         header : 'How long does it take to complete a project with SquareUp?',
-        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+        desc : "The duration of a project with our company varies depending on the project's complexity and scope. Generally, smaller projects can be completed within a few weeks, while more comprehensive projects may take several months. During our initial consultation, we will discuss your specific needs and objectives to provide a more accurate timeline. Our team is committed to delivering high-quality results efficiently, and we will keep you informed throughout the process to ensure timely and successful project completion.",
     },
     {
         number: '05',
         header : 'Do you offer ongoing support and maintenance after the project is completed?',
-        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+        desc : "SquareUp offers ongoing support and maintenance after the project is completed. Our commitment to your success doesn't end with the delivery of the project. We provide a range of post-project services including regular updates, troubleshooting, performance monitoring, and any necessary adjustments to ensure your solution continues to operate smoothly and effectively. Our dedicated support team is always available to assist you, ensuring that your business continues to benefit from our services long after the project has been completed.",
     },
     {
         number: '06',
         header : 'Can you work with existing design or development frameworks?',
-        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+        desc : 'Absolutely, SquareUp can work with existing design or development frameworks. We understand that many businesses have established systems and processes in place, and we are experienced in integrating our solutions with a variety of frameworks. Our team is adept at adapting to different environments and can seamlessly collaborate with your current setup to enhance and build upon what you already have. Whether you need to refine your existing framework or incorporate new features, we have the expertise to ensure a smooth and efficient integration process.',
     },
     {
         number: '07',
         header : 'How involved will I be in the project development process?',
-        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+        desc : 'Your involvement in the project development process is crucial to its success, and we highly encourage active participation from our clients. From the initial consultation to the final delivery, we maintain open lines of communication to ensure that your vision and feedback are integrated at every stage. We conduct regular meetings and updates to keep you informed of progress, discuss any adjustments, and ensure that the project aligns with your expectations. Your insights and input are invaluable, and we work collaboratively with you to achieve the best possible outcome.',
     },
     {
         number: '08',
         header : 'Can you help with website or app maintenance and updates?',
-        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+        desc : 'Yes, we can help with website or app maintenance and updates. We offer comprehensive maintenance services to ensure your website or app remains up-to-date, secure, and fully functional. Our team handles everything from regular updates and bug fixes to performance enhancements and security patches. By entrusting us with the ongoing maintenance of your digital platforms, you can focus on running your business while we ensure that your website or app continues to deliver an optimal user experience.',
     },
 ]
 
