f761f00f9e483867a246193a4a26626376231b07
Christofer Bryan <christoferbryan45@gmail.com>
2024-05-31 15:16:09 +0700

    completed FAQ section of home page

--- src/app/components/Faq/index.tsx
+++ src/app/components/Faq/index.tsx
@@ -1,17 +1,50 @@
+import FaqData from "@/utils/Faq";
+import Accordion from "./Accordion";
+
 const Faq : React.FC = () => {
     return (
         <>
             <div className="bg-[#1A1A1A] flex flex-col items-center">
+
                 <div className="bg-zinc-900 text-center flex flex-col justify-center items-center w-[350px] h-[220px] px-[16px] py-[50px] gap-y-[6px]
                 border-b border-zinc-600 lg:w-[1280px] lg:h-[280px] lg:gap-y-[10px] lg:px-[250px] lg:py-[100px]">
-                    <h2 className="text-white font-semi-bold text-[28px]">Frequently Asked Questions</h2>
-                    <p className="text-[#E6E6E6] font-normal text-[14px]">
-                        Still you have any questions? Contact our Team via hello@squareup.com
+                    <h2 className="text-white font-semi-bold text-[28px] lg:text-[38px]">Frequently Asked Questions</h2>
+                    <p className="text-[#E6E6E6] font-normal text-[14px] lg:text-[16px]">
+                        Still have any questions? Contact our Team via hello@squareup.com
                     </p>
                 </div>
-                <div className='flex flex-col items-center'>
 
+                <div className='flex flex-col lg:hidden'>
+                    {
+                        FaqData.map((question, index)=>(
+                            <div key={index} className="flex items-center justify-center">
+                                <Accordion {...question} />        
+                            </div>
+                        ))
+                    }
                 </div>
+
+                <div className="w-[1280px] gap-x-[0px] hidden lg:flex">
+                    <div className='flex flex-col'>
+                        {
+                            FaqData.slice(0,4).map((question, index)=>(
+                                <div key={index} className="flex items-center justify-center">
+                                    <Accordion {...question} />        
+                                </div>
+                            ))
+                        }
+                    </div>
+                    <div className='flex flex-col'>
+                        {
+                            FaqData.slice(4,8).map((question, index)=>(
+                                <div key={index} className="flex items-center justify-center">
+                                    <Accordion {...question} />        
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                
             </div>
         </>
     );
---
+++ src/utils/Faq.ts
@@ -0,0 +1,44 @@
+const FaqData = [
+    {
+        number: '01',
+        header : 'What services does SquareUp provide?',
+        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+    },
+    {
+        number: '02',
+        header : 'How can SquareUp help my business?',
+        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+    },
+    {
+        number: '03',
+        header : 'What industries does SquareUp work with?',
+        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+    },
+    {
+        number: '04',
+        header : 'How long does it take to complete a project with SquareUp?',
+        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+    },
+    {
+        number: '05',
+        header : 'Do you offer ongoing support and maintenance after the project is completed?',
+        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+    },
+    {
+        number: '06',
+        header : 'Can you work with existing design or development frameworks?',
+        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+    },
+    {
+        number: '07',
+        header : 'How involved will I be in the project development process?',
+        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+    },
+    {
+        number: '08',
+        header : 'Can you help with website or app maintenance and updates?',
+        desc : 'SquareUp offers a range of services including design, engineering, and project management. We specialize in user experience design, web development, mobile app development, custom software development, branding and identity, and more.',
+    },
+]
+
+export default FaqData;
